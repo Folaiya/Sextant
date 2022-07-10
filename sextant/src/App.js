a48d586e4159bb40d1d0e33aefc10b43c2429d25
Folaiya <folatech21@gmail.com>
2022-07-10 19:42:33 +0100

    added some component to display user IP address

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,19 +1,37 @@
 
 import './App.css';
+import {useState,useEffect} from 'react'
+
 
 function App() {
+  const [ip, setIP] = useState('');
+
+  //creating function to load ip address from the API
+  const getData = async () => {
+    const response = await fetch('https://api64.ipify.org?format=json')
+    .then((response) => response.json())
+    .then(data => console.log(data));
+    console.log(response.data);
+    setIP(response.data.IPV4);
+    
+  }
+  
+  useEffect( () => {
+    //passing getData method to the lifecycle method
+    getData()
+
+  }, []);
+
   return (
     <div className="App">
       <h1>Sextant World</h1>
       <h3>You Are Welcome</h3>
-      
-      
-    
-      
-    
+      <h5>Your IP Address is</h5>
+      <h6>{ip}</h6>
       
       
     </div>
+    
   );
 }
 
